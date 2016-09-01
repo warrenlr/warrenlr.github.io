ecfca30507cb41b6d942a045d4169bb3a9f3380d
Rene Warren <rwarren@bcgsc.ca>
2016-09-01 10:18:28 -0700

    updates

--- renewarren.ca/About_files/About.js
+++ renewarren.ca/About_files/About.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20160831
+// Created by iWeb 3.0.4 local-build-20160901
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('About_files/AboutMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();IMpreload('About_files','shapeimage_3','0');IMpreload('About_files','shapeimage_3','1');IMpreload('About_files','shapeimage_3','2');IMpreload('About_files','shapeimage_5','0');IMpreload('About_files','shapeimage_5','1');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');Widget.onload();fixupAllIEPNGBGs();IMpreload('About_files','shapeimage_6','0');IMpreload('About_files','shapeimage_6','1');IMpreload('About_files','shapeimage_6','2');IMpreload('About_files','shapeimage_8','0');IMpreload('About_files','shapeimage_8','1');applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
