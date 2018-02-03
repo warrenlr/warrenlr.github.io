caeea8d9bbe1b95d01511071808af21339ac48bb
Rene Warren <rwarren@bcgsc.ca>
2018-02-03 11:46:29 -0800

    fix site

--- renewarren.ca/About_files/About.js
+++ renewarren.ca/About_files/About.js
@@ -6,4 +6,4 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('About_files/AboutMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');fixAllIEPNGs('Media/transparent.gif');IMpreload('About_files','shapeimage_2','0');IMpreload('About_files','shapeimage_2','1');IMpreload('About_files','shapeimage_2','2');IMpreload('About_files','shapeimage_2','3');IMpreload('About_files','shapeimage_2','4');IMpreload('About_files','shapeimage_2','5');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
