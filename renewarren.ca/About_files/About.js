1ec89a6688d7a58d74e29fc47fe973a5f3dc70f1
Rene Warren <rwarren@bcgsc.ca>
2018-02-04 08:42:10 -0800

    publish site changes

--- renewarren.ca/About_files/About.js
+++ renewarren.ca/About_files/About.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20180203
+// Created by iWeb 3.0.4 local-build-20180204
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
@@ -6,4 +6,4 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('About_files/AboutMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
