5f28b4026dce33ef0124560977913041dcc2f626
Rene Warren <rwarren@bcgsc.ca>
2018-06-02 08:35:29 -0700

    update site

--- renewarren.ca/Shanti_birth_files/Shanti_birth.js
+++ renewarren.ca/Shanti_birth_files/Shanti_birth.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20180304
+// Created by iWeb 3.0.4 local-build-20180602
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWPhotoFrame([IWCreateImage('Shanti_birth_files/FormalShadow_01.png'),IWCreateImage('Shanti_birth_files/FormalShadow_02.png'),IWCreateImage('Shanti_birth_files/FormalShadow_03.png'),IWCreateImage('Shanti_birth_files/FormalShadow_06.png'),IWCreateImage('Shanti_birth_files/FormalShadow_12.png'),IWCreateImage('Shanti_birth_files/FormalShadow_11.png'),IWCreateImage('Shanti_birth_files/FormalShadow_10.png'),IWCreateImage('Shanti_birth_files/FormalShadow_04.png')],null,2,0.652105,1.000000,5.000000,1.000000,4.000000,17.000000,17.000000,17.000000,36.000000,4.000000,837.000000,4.000000,837.000000,null,null,null,0.100000)});registry.applyEffects();}
