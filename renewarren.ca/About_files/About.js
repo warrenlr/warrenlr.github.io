b1f5e81af8a8000065aec8e342f0238fa151bc6e
Rene Warren <rwarren@bcgsc.ca>
2019-04-24 18:04:34 -0700

    update to profile

--- renewarren.ca/About_files/About.js
+++ renewarren.ca/About_files/About.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20190328
+// Created by iWeb 3.0.4 local-build-20190424
 
 setTransparentGifURL('Media/transparent.gif');function applyEffects()
 {var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
