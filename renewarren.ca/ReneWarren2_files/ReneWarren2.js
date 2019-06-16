4adfc2b3a59ba1474b9403b33a39a0c2a31101d8
Rene Warren <rwarren@bcgsc.ca>
2019-06-16 11:22:46 -0700

    update web

---
+++ renewarren.ca/ReneWarren2_files/ReneWarren2.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20190616
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('ReneWarren2_files/ReneWarren2Moz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
