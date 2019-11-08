a2a1d674f5fdbe0086b59207a96cfac1034016ef
Rene Warren <rwarren@bcgsc.ca>
2019-11-07 16:24:49 -0800

    update to site

--- renewarren.ca/HLAminer_files/HLAminer.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20191026
-
-setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('HLAminer_files/HLAminerMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();fixAllIEPNGs('Media/transparent.gif');Widget.onload();IMpreload('HLAminer_files','shapeimage_2','0');performPostEffectsFixups()}
-function onPageUnload()
-{Widget.onunload();}
--- rlw/About_files/About.js
+++ rlw/About_files/About.js
@@ -6,6 +6,6 @@ function hostedOnDM()
 {return false;}
 function onPageLoad()
 {loadMozillaCSS('About_files/AboutMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');fixAllIEPNGs('Media/transparent.gif');Widget.onload();applyEffects()}
 function onPageUnload()
 {Widget.onunload();}
