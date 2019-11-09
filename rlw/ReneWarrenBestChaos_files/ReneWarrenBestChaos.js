14d57291b569a94072cf9d73270f1f39f4487f3a
Rene Warren <rwarren@bcgsc.ca>
2019-11-09 15:04:15 -0800

    update

--- rlw/ReneWarrenBestChaos_files/ReneWarrenBestChaos.js
+++
@@ -1,9 +0,0 @@
-// Created by iWeb 3.0.4 local-build-20191108
-
-setTransparentGifURL('Media/transparent.gif');function applyEffects()
-{var registry=IWCreateEffectRegistry();registry.registerEffects({stroke_0:new IWEmptyStroke()});registry.applyEffects();}
-function hostedOnDM()
-{return false;}
-function onPageLoad()
-{loadMozillaCSS('ReneWarrenBestChaos_files/ReneWarrenBestChaosMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');fixAllIEPNGs('Media/transparent.gif');applyEffects()}
